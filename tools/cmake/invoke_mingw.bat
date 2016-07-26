69b678a3b2dc71e85e292abbca7bcf81cd576aa9
Toshiyuki-Yamashita <Toshiyuki-Yamashita@users.noreply.github.com>
2016-07-26 14:27:47 +0900

    simplify batch-file
    use labels

--- tools/cmake/invoke_mingw.bat
+++ tools/cmake/invoke_mingw.bat
@@ -1,56 +1,30 @@
 @echo off 
 
-set buildroot=%~1
-set buildtype=%~2
-
-if "%3" == "" (
-    set buildconfig=Debug
-)
-if "%3" == "debug" (
-    set buildconfig=Debug
-)
-if "%3" == "release" (
-    set buildconfig=Release
-)
-
 set MINGW_PATH=C:\usr\share\ruby\ruby\devkit-mingw64-64\mingw\bin
-set builddir=%buildroot%\build
-set srcdir=%buildroot%\src
 path=C:\usr\share\CMake\bin;%MINGW_PATH%
 
 
+goto :%buildtype%
 
-if "%buildtype%" == "generate" (
+:generate
     if not exist "%builddir%" ( mkdir "%builddir%" )
     set arg=-G "MinGW Makefiles" -D CMAKE_PROGRAM_PATH=%MINGW_PATH% -D CMAKE_BUILD_TYPE=%buildconfig% "%srcdir%" 
-)
+    goto :cmake 
 
-if "%buildtype%" == "build" (
-    if not exist "%builddir%\Makefile" (call %0 %1 generate %3)
+:build
     set arg=--build %builddir% --config %buildconfig%
-)
+    goto :cmake
 
-if "%buildtype%" == "distclean" (
+:distclean
     rmdir /q /s "%builddir%"
     exit /b 0
-)
 
-if "%buildtype%" == "clean" (
+:clean
     set arg=--build %builddir% --target clean
-)
+    goto :cmake
 
-if "%buildtype%" == "regenerate" (
-    call %0 %1 distclean %3
-    call %0 %1 generate %3
-    exit /b 0
-)
 
-if "%buildtype%" == "rebuild" (
-    call %0 %1 clean %3
-    call %0 %1 build %3
-    exit /b 0
-)
-
+:cmake
 pushd %builddir%
 cmake  %arg% 
 popd 
